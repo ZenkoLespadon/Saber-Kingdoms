8e0b40af4f339bd1d5053f62a35923d59bfa641e
ZenkoLespadon <lefebvre.william82@gmail.com>
2026-02-18 01:17:18 +0200

    feat: modify ListCommand to sort with members

--- src/main/java/com/kingdomspvp/kingdoms/commands/ListCommand.java
+++ src/main/java/com/kingdomspvp/kingdoms/commands/ListCommand.java
@@ -4,9 +4,12 @@ import com.kingdomspvp.kingdoms.model.Kingdom;
 import com.kingdomspvp.kingdoms.model.KPlayer;
 import com.kingdomspvp.kingdoms.services.KPlayerManager;
 import com.kingdomspvp.kingdoms.services.KingdomsManager;
+import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
 import org.bukkit.ChatColor;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,7 +18,7 @@ public class ListCommand extends KingdomCommand {
 
     public ListCommand() {
         this.aliases.add("list");
-        this.helpShort = "Affiche la liste des royaumes existants triés par puissance";
+        this.helpShort = "Affiche la liste des royaumes existants triés par nombre de membres";
     }
 
     @Override
@@ -27,40 +30,42 @@ public class ListCommand extends KingdomCommand {
             return;
         }
 
-        // Calculer le power total de chaque royaume
-        Map<Kingdom, Integer> kingdomPowerMap = kingdoms.stream()
-                .collect(Collectors.toMap(
-                        kingdom -> kingdom,
-                        this::getKingdomPower
-                ));
+        // Liste de tous les FPlayers
+        List<FPlayer> allPlayers = (List<FPlayer>) FPlayers.getInstance().getAllFPlayers();
 
-        // Trier les royaumes par power décroissant
-        List<Map.Entry<Kingdom, Integer>> sortedKingdoms = kingdomPowerMap.entrySet().stream()
+        // Associe chaque royaume à son nombre total de membres
+        Map<Kingdom, Integer> countMap = new HashMap<>();
+
+        for (Kingdom kingdom : kingdoms) {
+            List<Faction> factions = kingdom.getFactions();
+            if (factions == null || factions.isEmpty()) {
+                countMap.put(kingdom, 0);
+                continue;
+            }
+
+            int count = (int) allPlayers.stream()
+                    .filter(fp -> fp.getFaction() != null && factions.contains(fp.getFaction()))
+                    .count();
+
+            countMap.put(kingdom, count);
+        }
+
+        // Tri par nombre de membres décroissant
+        List<Map.Entry<Kingdom, Integer>> sorted = countMap.entrySet().stream()
                 .sorted(Map.Entry.<Kingdom, Integer>comparingByValue().reversed())
                 .collect(Collectors.toList());
 
-        context.msg(ChatColor.GREEN + "Classement des royaumes par puissance :");
-        for (Map.Entry<Kingdom, Integer> entry : sortedKingdoms) {
-            Kingdom kingdom = entry.getKey();
-            int power = entry.getValue();
-            context.msg(ChatColor.YELLOW + "- " + ChatColor.GREEN + kingdom.getName()
-                    + ChatColor.WHITE + " (Couleur: " + kingdom.getColor() + kingdom.getColor().name()
-                    + ChatColor.WHITE + ", Power: " + ChatColor.GOLD + power + ChatColor.WHITE + ")");
-        }
-    }
+        context.msg(ChatColor.GREEN + "Classement des royaumes par nombre de membres :");
 
-    // Calcule la puissance totale d'un royaume
-    private int getKingdomPower(Kingdom kingdom) {
-        return KPlayerManager.getSortedKPlayers().stream()
-                .filter(kPlayer -> kingdom.getFactions().stream()
-                        .anyMatch(faction ->
-                                FPlayers.getInstance().getAllFPlayers().stream()
-                                        .filter(fPlayer -> fPlayer.getFaction().equals(faction))
-                                        .anyMatch(fPlayer -> fPlayer.getPlayer() != null && fPlayer.getPlayer().equals(kPlayer.getPlayer()))
-                        )
-                )
-                .mapToInt(KPlayer::getPower)
-                .sum();
+        for (Map.Entry<Kingdom, Integer> entry : sorted) {
+            Kingdom k = entry.getKey();
+            int members = entry.getValue();
+            ChatColor col = k.getColor();
+
+            context.msg(" - " + col + k.getName()
+                    + ChatColor.WHITE + " ("
+                    + ChatColor.GOLD + members + " membres" + ChatColor.WHITE + ")");
+        }
     }
 
     @Override
